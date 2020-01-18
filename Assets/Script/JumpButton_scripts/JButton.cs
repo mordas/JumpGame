cd45940c57eb05e41389ac90660a91b960a93f4a
dMordas <mordasdsp@gmail.com>
2020-01-18 17:20:23 +0300

    add game scene and script templates for jump

---
+++ Assets/Script/JumpButton_scripts/JButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+ public void OnPointerDown(PointerEventData data){
+     if(PlayerJump.instance != null){
+         PlayerJump.instance.SetPower(true);
+     }
+ }
+ public void OnPointerUp(PointerEventData data){
+     if(PlayerJump.instance != null){
+         PlayerJump.instance.SetPower(false);
+     }
+ }
+}
---
+++ Assets/Script/Player_scripts/PlayerJump.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public static PlayerJump instance;
+    private Rigidbody2D _myBody;
+    private Animator _anim;
+    private float _forceX, _forceY;
+    private float _tresholdX = 7.0f;
+    private float _tresholdY = 14.0f;
+    private bool _setPower, _didJump;
+
+    void Awake()
+    {
+        MakeInstance();
+    }
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void SetPower(bool _setPower)
+    {
+        this._setPower = _setPower;
+        if (_setPower)
+        {
+            Debug.Log("We are setting power");
+        }
+        else
+        {
+
+            Debug.Log("We are not setting power");
+        }
+
+    }
+}
